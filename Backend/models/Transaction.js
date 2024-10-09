e0ca7bb84e359340d7b01cb531ddb996a69dab33
sushant-sonawane121 <sushantsonawane121@gmail.com>
2024-10-09 15:33:09 +0530

    first commit

---
+++ Backend/models/Transaction.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const TransactionSchema = new mongoose.Schema({
+  title: String,
+  price: Number,
+  description: String,
+  category: String,
+  image: String,
+  sold: Boolean,
+  dateOfSale: Date,
+});
+
+module.exports = mongoose.model("Transaction", TransactionSchema);
